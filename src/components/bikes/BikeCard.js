4c25b30a9efd454ac6ca9ea81385403447f877fb
stu-wd <stuart.darsey@gmail.com>
2022-01-26 15:00:45 -0500

    bikecard additions

--- src/components/bikes/BikeCard.js
+++ src/components/bikes/BikeCard.js
@@ -3,12 +3,24 @@ import { Link } from "react-router-dom";
 import { useBikes } from "../../state/bikesContext";
 
 const BikeCard = (props) => {
-  const { brand, status, style, serial, size, storage, wobc_id } = props.match;
+  const {
+    brand,
+    status,
+    style,
+    serial,
+    size,
+    storage,
+    wobc_id,
+    gender,
+    adultchild,
+  } = props.match;
   return (
     <div className="border-[1px] border-grey p-2 mb-2">
       <h5>serial: {serial}</h5>
       <h5>wobc_id: {wobc_id}</h5>
       <h5>status: {status}</h5>
+      <h5>gender: {gender}</h5>
+      <h5>adultchild: {adultchild}</h5>
       <h5>size: {size}</h5>
       <h5>brand: {brand}</h5>
       <h5>style: {style}</h5>
